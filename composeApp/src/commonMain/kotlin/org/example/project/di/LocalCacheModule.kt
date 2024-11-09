458a9bb382cdc79c6e9e87117581052600d59ae2
Steve <steve@kitkat@gmail.com>
2024-11-09 20:49:47 +0700

    fixes issue with the start koin

--- composeApp/src/commonMain/kotlin/org/example/project/di/LocalCacheModule.kt
+++ composeApp/src/commonMain/kotlin/org/example/project/di/LocalCacheModule.kt
@@ -5,12 +5,13 @@ import io.realm.kotlin.RealmConfiguration
 import org.example.project.alarm.domain.AlarmDataSource
 import org.example.project.alarm.data.tables.AlarmTable
 import org.example.project.alarm.data.LocalAlarmDataSourceImp
+import org.example.project.core.data.local_client.RealmDbClient
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.bind
 import org.koin.dsl.module
 
 val localCacheModule = module {
-    single { provideRealm() }
+    single { RealmDbClient() }
     singleOf(::LocalAlarmDataSourceImp).bind<AlarmDataSource>()
 }
 
