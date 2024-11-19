92957f97b1b989de5e2e4cd678028317eb5ee39b
graial <al@paglalayag.net>
2024-11-19 14:37:42 +0800

    remove unneeded imports

--- composeApp/src/commonTest/kotlin/FakeClock.kt
+++ composeApp/src/commonTest/kotlin/FakeClock.kt
@@ -2,22 +2,15 @@ package org.example.project
 
 import kotlinx.datetime.Clock
 import kotlinx.datetime.Instant
-import kotlinx.datetime.LocalDate
-import kotlinx.datetime.LocalTime
-import kotlinx.datetime.UtcOffset
-import kotlinx.datetime.format.DateTimeComponents
-import kotlinx.datetime.format.char
 
-class FakeClock : Clock {
+class Clock(private val fixedInstant: Instant) : Clock {
+    class System(private val fixedInstant: Instant) : Clock {
+        override fun now(): Instant = fixedInstant
+    }
+
     override fun now(): Instant {
-        val customFormat = DateTimeComponents.Format {
-            date(LocalDate.Formats.ISO)
-            char(' ')
-            time(LocalTime.Formats.ISO)
-            char(' ')
-            offset(UtcOffset.Formats.ISO)
-        }
-        val instant = Instant.parse("2023-01-02 22:35:01.14 +01:00", customFormat)
-        return instant
+        TODO("Not yet implemented")
     }
 }
+
+fun Clock.Companion.System(fixedInstant: Instant): Clock = Clock(fixedInstant)
